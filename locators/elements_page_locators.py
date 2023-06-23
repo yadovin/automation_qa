8452672588ab9bf4f8a18002f16d83e3f4bc2040
Даниил <yadovin.daniil99@yandex.ru>
2023-06-23 22:03:11 +0300

    fixed go_to_element in BasePage update elements tests added test checkboxes

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -1,8 +1,9 @@
+from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 
 
 class TextBoxPageLocators:
-    #form fields
+    # form fields
 
     FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
     EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
@@ -10,9 +11,17 @@ class TextBoxPageLocators:
     PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
     SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
 
-    #create from
+    # create from
 
     CREATE_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
     CREATE_EMAIL = (By.CSS_SELECTOR, '#output #email')
     CREATE_CURRENT_ADDRESS = (By.CSS_SELECTOR, '#output #currentAddress')
     CREATE_PERMANENT_ADDRESS = (By.CSS_SELECTOR, '#output #permanentAddress')
+
+
+class CheckBoxPageLocators:
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
+    ITEM_LIST = (By.CSS_SELECTOR, "span[class='rct-title']")
+    CHECKED_ITEMS = (By.CSS_SELECTOR, "svg[class='rct-icon rct-icon-check']")
+    TITLE_ITEM = ".//ancestor::span[@class='rct-text']"
+    OUTPUT_RESULT = (By.CSS_SELECTOR, "span[class='text-success']")
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,7 +1,8 @@
+import random
 import time
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
 from pages.base_page import BasePage
 
 
@@ -21,10 +22,45 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
         return full_name, email, current_address, permanent_address
+
     def check_filled_form(self):
         full_name = self.element_is_visible(self.locators.CREATE_FULL_NAME).text.split(':')[1]
         email = self.element_is_visible(self.locators.CREATE_EMAIL).text.split(':')[1]
-        current_address= self.element_is_visible(self.locators.CREATE_CURRENT_ADDRESS).text.split(':')[1]
+        current_address = self.element_is_visible(self.locators.CREATE_CURRENT_ADDRESS).text.split(':')[1]
         permanent_address = self.element_is_visible(self.locators.CREATE_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
 
+
+class CheckBoxPage(BasePage):
+    locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_checkbox(self):
+        item_list = self.elements_are_visible(self.locators.ITEM_LIST)
+        count = 21
+        while count != 0:
+            item = item_list[random.randint(1, 15)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                print(item.text)
+                count -= 1
+            else:
+                break
+
+    def get_checked_checkboxes(self):
+        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)
+        data = []
+        for box in checked_list:
+            title_item = box.find_element("xpath", self.locators.TITLE_ITEM)
+            data.append(title_item.text)
+        return str(data).replace(' ', '').replace('doc', '').replace('.', '').lower()
+
+    def get_output_result(self):
+        result_list = self.elements_are_present(self.locators.OUTPUT_RESULT)
+        data = []
+        for item in result_list:
+            data.append(item.text)
+        return str(data).replace(' ', '').lower()
