bad58f9036171dd9c60adf3aa75d34f425cb94e0
Даниил <yadovin.daniil99@yandex.ru>
2023-08-22 21:33:12 +0300

    update elements tests added class:    TestAccordianPage added tests:  - test_accordian

---
+++ tests/widgets_test.py
@@ -0,0 +1,14 @@
+from pages.widgets_page import AccordianPage
+from conftest import driver
+
+class TestWidgets:
+    class TestAccordianPage:
+        def test_accordian(self, driver):
+            accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
+            accordian_page.open()
+            first_title, first_content = accordian_page.check_accordian('first')
+            second_title, second_content = accordian_page.check_accordian('second')
+            third_title, third_content = accordian_page.check_accordian('third')
+            assert first_title == 'What is Lorem Ipsum?' and first_content > 0
+            assert second_title == 'Where does it come from?' and second_content > 0
+            assert third_title == 'Why do we use it?' and third_content > 0
