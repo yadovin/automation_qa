d7ebb76fda242e2a67af561704fa2f9cc28bbc86
Даниил <yadovin.daniil99@yandex.ru>
2023-08-22 20:54:45 +0300

    update elements tests added class:    TestModalDialogsPage added tests:  - test_modal_dialogs

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,5 +1,6 @@
 import time
-from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramePage, NestedFramesPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramePage, NestedFramesPage, \
+    ModalDialogsPage
 from conftest import driver
 
 
@@ -64,3 +65,14 @@ class TestNestedFramesPage:
         parent_text, child_text = nested_frame_page.check_nested_frame()
         assert parent_text == "Parent frame", 'Nested frame does not exist'
         assert child_text == "Child Iframe", 'Nested frame does not exist'
+
+
+class TestModalDialogsPage:
+
+    def test_modal_dialogs(self, driver):
+        modal_dialogs_page = ModalDialogsPage(driver, 'https://demoqa.com/modal-dialogs')
+        modal_dialogs_page.open()
+        small, large = modal_dialogs_page.check_modal_dialogs()
+        assert small[1] < large [1], 'text from large dialogs is less than text from small dialog'
+        assert small[0] == 'Small Modal', 'The header is not "Small modal"'
+        assert large[0] == 'Large Modal', 'The header is not "Large modal'
